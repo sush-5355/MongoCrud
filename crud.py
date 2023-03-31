ecaa66bda09c781a97e913d46c9ab6492db0e3b7
SUSHANT SUMAN <81477161+sush-5355@users.noreply.github.com>
2023-04-01 04:17:15 +0530

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,10 +1,8 @@
-from utilities.date_conversion import convert_date
 from bson.objectid import ObjectId
-from utilities.mongo_config import *
+from mongo_utilities.date_conversion import convert_date
+from mongo_utilities.mongo_config import db
 
-
-def create(client, database_name: str, collection_name: str, data: str):
-    db = client[database_name]
+async def create(collection_name: str, data: dict):
     collection = db[collection_name]
     data.update({'created_at': convert_date(), 'updated_at': convert_date()})
     response = collection.insert_one(data)
@@ -15,8 +13,8 @@ def create(client, database_name: str, collection_name: str, data: str):
     return dict(status=True, data=document)
 
 
-def get_via_id(client, database_name: str, collection_name: str, id: str):
-    db = client[database_name]
+async def get_via_id(collection_name: str, id: str):
+
     collection = db[collection_name]
     query = {"_id": ObjectId(id)}
     # retrieve the document
@@ -28,21 +26,21 @@ def get_via_id(client, database_name: str, collection_name: str, id: str):
         return dict(status=False, msg=str(e))
 
 
-def update_via_id(client, database_name: str, collection_name: str, id: str, mapping: dict):
-    db = client[database_name]
+async def update_via_id(collection_name: str, id: str, mapping: dict):
+
     collection = db[collection_name]
     query = {'_id': ObjectId(id)}
     mapping.update({'updated_at': convert_date()})
     new_values = {'$set': mapping}
     updated_document = collection.update_one(query, new_values)
     if updated_document is not None:
-        return get_via_id(client=client, database_name=database_name, collection_name=collection_name, id=id)
+        return await get_via_id(collection_name=collection_name, id=id)
     else:
         return dict(status=False, msg='No record found')
 
 
-def update(client, database_name: str, collection_name: str, query: dict, mapping: dict):
-    db = client[database_name]
+async def update(collection_name: str, query: dict, mapping: dict):
+
     collection = db[collection_name]
     mapping.update({'updated_at': convert_date()})
     new_values = {'$set': mapping}
@@ -51,35 +49,35 @@ def update(client, database_name: str, collection_name: str, query: dict, mappin
         updated_documents = collection.find(mapping)
         response_list = []
         for document in updated_documents:
-            print("Updated document _id:", document['_id'])
-            response = get_via_id(client=client, database_name=database_name,
-                                  collection_name=collection_name, id=document['_id'])
+            response = await get_via_id(collection_name=collection_name, id=document['_id'])
             response_list.append(response)
         return dict(status=True, data=response_list, count=result.modified_count)
     except Exception as e:
         return dict(status=False, msg=str(e))
 
 
-def get_all(client, database_name: str, collection_name: str):
-    db = client[database_name]
+async def get_all(collection_name: str, query:dict=None):
+
     collection = db[collection_name]
     # retrieve all documents in the collection
-
-    documents = collection.find({})
+    if query:
+        documents = collection.find(query)
+        count = collection.count_documents(query)
+    else:
+        documents = collection.find({})
+        count = collection.count_documents({})
     all_documents = []
     # iterate over the cursor and print each document
     for document in documents:
         document['_id'] = str(document['_id'])
-        print(document)
         all_documents.append(document)
-    count = collection.count_documents({})
     if count > 0:
         return dict(status=True, data=all_documents, count=count)
-    return dict(status=False, msg='Please check the database and collection name', count=count)
+    return dict(status=False, msg='No Data found', count=count)
 
 
-def delete_via_query(client, database_name: str, collection_name: str, query={}):
-    db = client[database_name]
+async def delete_via_query(collection_name: str, query={}):
+
     collection = db[collection_name]
     try:
         result = collection.delete_many(query)
@@ -88,13 +86,11 @@ def delete_via_query(client, database_name: str, collection_name: str, query={})
         return dict(status=False, msg=str(e))
 
 
-def del_via_id(client, database_name: str, collection_name: str, id: str):
-    db = client[database_name]
+async def del_via_id(collection_name: str, id: str):
+
     collection = db[collection_name]
-
-    # define the _id of the record to delete
+    # async define the _id of the record to delete
     record_id = ObjectId(id)
-
     # delete the record with the specified _id
     try:
         collection.delete_one({'_id': record_id})
